d11626e8981fb70997f708394a20457ccfeadba2
Revers <0x00stone@gmail.com>
2024-03-02 19:48:47 +0800

    change markdown name to README.md

--- behavioral/observer/observer.java
+++ behavioral/observer/observer.java
@@ -8,71 +8,62 @@ import java.util.List;
  * @date 2024/02/24 23:04
  **/
 public class observer {
-    // 观察者接口
-    interface Observer {
+    public interface Subject {
+        // 注册观察者
+        void registerObserver(Observer observer);
+        // 移除观察者
+        void removeObserver(Observer observer);
+        // 通知观察者
+        void notifyObservers(String message);
+    }
+
+    public interface Observer {
+        // 更新消息
         void update(String message);
     }
 
-    // 具体的观察者
-    class ConcreteObserver implements Observer {
-        private String observerName;
+    public class ConcreteSubject implements Subject {
+        private List<Observer> observers = new ArrayList<Observer>();
 
-        public ConcreteObserver(String observerName) {
-            this.observerName = observerName;
+        @Override
+        public void registerObserver(Observer observer) {
+            observers.add(observer);
         }
 
         @Override
-        public void update(String message) {
-            System.out.println(observerName + " received message: " + message);
-        }
-    }
-
-    // 被观察的主题接口
-    interface Subject {
-        void attach(Observer o);
-        void detach(Observer o);
-        void notifyUpdate(String message);
-    }
-
-    // 具体的主题
-    class ConcreteSubject implements Subject {
-        private List<Observer> observers = new ArrayList<>();
-
-        @Override
-        public void attach(Observer o) {
-            observers.add(o);
+        public void removeObserver(Observer observer) {
+            observers.remove(observer);
         }
 
         @Override
-        public void detach(Observer o) {
-            observers.remove(o);
-        }
-
-        @Override
-        public void notifyUpdate(String message) {
+        public void notifyObservers(String message) {
             for (Observer observer : observers) {
                 observer.update(message);
             }
         }
+
     }
 
-    // 演示
+    public class ConcreteObserverOne implements Observer {
+        @Override
+        public void update(String message) {
+            //TODO: 获取消息通知，执行自己的逻辑...
+            System.out.println("ConcreteObserverOne is notified.");
+        }
+    }
+
+    public class ConcreteObserverTwo implements Observer {
+        @Override
+        public void update(String message) {
+            //TODO: 获取消息通知，执行自己的逻辑...
+            System.out.println("ConcreteObserverTwo is notified.");
+        }
+    }
+
     public static void main(String[] args) {
         ConcreteSubject subject = new observer().new ConcreteSubject();
-
-        Observer observer1 = new observer().new ConcreteObserver("Observer 1");
-        Observer observer2 = new observer().new ConcreteObserver("Observer 2");
-        Observer observer3 = new observer().new ConcreteObserver("Observer 3");
-
-        subject.attach(observer1);
-        subject.attach(observer2);
-        subject.attach(observer3);
-
-        subject.notifyUpdate("The Subject has sent a message!");
-
-        //Observer 1 received message: The Subject has sent a message!
-        //Observer 2 received message: The Subject has sent a message!
-        //Observer 3 received message: The Subject has sent a message!
+        subject.registerObserver(new observer().new ConcreteObserverOne());
+        subject.registerObserver(new observer().new ConcreteObserverTwo());
+        subject.notifyObservers("hello");
     }
-
 }
